d0d912e2d141ea5ddf6b690b031402222176041f
Mychael M <mychaelbmenges@gmail.com>
2020-11-01 17:50:06 -0700

    added protected route to friendsList page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+import FriendsList from './components/FriendsList';
 import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
  
 function App() {
   return (
@@ -11,9 +13,13 @@ function App() {
         <li>
           <Link to="/login">Login</Link>
         </li>
+        <li>
+          <Link to="/friendsList">Friends</Link>
+        </li>
       </ul>
       <h1>Auth-Friends</h1>
       <Switch>
+        <PrivateRoute exact path="/friendsList" component={FriendsList} />
         <Route path="/login" component={Login} />
       </Switch>
 
---
+++ friends/src/components/FriendsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FriendsList = () => {
+
+  return (
+  <h2>This is the FriendsList Page</h2>
+  )
+}
+
+export default FriendsList;
